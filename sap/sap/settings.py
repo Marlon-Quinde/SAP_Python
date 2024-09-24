997dbeed4cbb4bb2a1755e4a43310de3b405fa5f
Marlon Quinde <71990962+Marlon-Quinde@users.noreply.github.com>
2024-09-23 23:36:15 -0500

    feat(Django-environ): :sparkles: Implementacion de varaibles de entorno en django

--- sap/sap/settings.py
+++ sap/sap/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+
+# Set the project base directory
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -85,11 +91,11 @@ WSGI_APPLICATION = 'sap.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sap_db',
-        'USER': 'alumno',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASS'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT')
     }
 }
 
