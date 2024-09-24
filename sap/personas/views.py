672b29321b1b3aa24ea5c793dbb2b7665d8be5b7
mquinde <mquinde@viamatica.me>
2024-09-24 17:31:26 -0500

    feat(POST/Persona): :sparkles: Implementacion del POST de personas en DJANGO

--- sap/personas/views.py
+++ sap/personas/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import get_object_or_404, render
+from django.forms import modelform_factory
+from django.shortcuts import get_object_or_404, redirect, render
 
 from personas.models import Persona
 
 # Create your views here.
+PersonaForm = modelform_factory(Persona, exclude=[])
+
+def nuevaPersona(request):
+    if request.method == 'POST':
+        formaPersona = PersonaForm(request.POST)
+        if formaPersona.is_valid():
+            formaPersona.save()
+            return redirect('index')
+    else:
+        formaPersona = PersonaForm()
+    return render(request, 'personas/nuevo.html', {'formaPersona': formaPersona})
+
+
 def detallePersona(request, id):
     # persona = Persona.objects.get(pk=id)
     persona = get_object_or_404(Persona, pk=id)
